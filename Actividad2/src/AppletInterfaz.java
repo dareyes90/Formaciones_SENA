55e25f505e2a8953beb36d1f26b34fdda4e1e25a
Deivy Reyes <deivyreyes90@gmail.com>
2018-10-17 10:43:38 -0500

    .

--- Actividad2/src/AppletInterfaz.java
+++ Actividad2/src/AppletInterfaz.java
@@ -6,18 +6,20 @@ public class AppletInterfaz extends Applet implements ItemListener {
 
 	Label l1;
 	List ch1;
-	Panel p1, p2;
+	Panel p1;
 
 	public AppletInterfaz() {
 		setLayout(new BorderLayout());
-		ch1 = new List();
+		p1 = new Panel(new FlowLayout());
+		ch1 = new List(3);
 		ch1.add("Gray");
 		ch1.add("Dark Gray");
 		ch1.add("Cyan");
 		ch1.add("Black");
+		p1.add(ch1);
 		l1 = new Label();
 		ch1.addItemListener(this);
-		add(ch1, BorderLayout.NORTH);	
+		add(p1, BorderLayout.NORTH);
 		add(l1, BorderLayout.CENTER);
 	}
 
@@ -25,15 +27,19 @@ public class AppletInterfaz extends Applet implements ItemListener {
 		switch (ch1.getSelectedIndex()) {
 		case 0:
 			l1.setBackground(Color.GRAY);
+			p1.setBackground(Color.GRAY);
 			break;
 		case 1:
 			l1.setBackground(Color.DARK_GRAY);
+			p1.setBackground(Color.DARK_GRAY);
 			break;
 		case 2:
 			l1.setBackground(Color.CYAN);
+			p1.setBackground(Color.CYAN);
 			break;
 		case 3:
 			l1.setBackground(Color.BLACK);
+			p1.setBackground(Color.BLACK);
 			break;
 		default:
 			break;
