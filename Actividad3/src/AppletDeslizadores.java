5b616d47f376b4f982bfa4289090ae210b3b3210
SURAMERICANA\deivremo <dareyes@sura.com.co>
2018-10-24 19:58:57 -0500

    Se agrega BotonesApplet

--- Actividad3/src/AppletDeslizadores.java
+++ Actividad3/src/AppletDeslizadores.java
@@ -1,6 +1,9 @@
 import java.applet.Applet;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.Panel;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
@@ -12,6 +15,7 @@ public class AppletDeslizadores extends Applet implements AdjustmentListener {
 	JScrollBar barra1, barra2, barra3;
 	Panel barras, fondo;
 	Color color;
+	Label deslizador1, deslizador2, deslizador3;
 
 	public AppletDeslizadores() {
 		setLayout(new BorderLayout());
@@ -19,12 +23,20 @@ public class AppletDeslizadores extends Applet implements AdjustmentListener {
 		barra2 = new JScrollBar(0, 0, 100, 0, 255);
 		barra3 = new JScrollBar(0, 0, 100, 0, 255);
 		barras = new Panel();
+		barras.setLayout(new GridLayout(1, 3));
 		fondo = new Panel();
+		fondo.setLayout(new GridLayout(3, 1, 10, 10));
+		deslizador1 = new Label("Deslizador 1");
+		deslizador2 = new Label("Deslizador 2");
+		deslizador3 = new Label("Deslizador 3");
 		barras.add(barra1);
 		barras.add(barra2);
 		barras.add(barra3);
-		add(barras,BorderLayout.NORTH);
-		add(fondo,BorderLayout.CENTER);
+		fondo.add(deslizador1);
+		fondo.add(deslizador2);
+		fondo.add(deslizador3);
+		add(barras, BorderLayout.NORTH);
+		add(fondo, BorderLayout.CENTER);
 		barra1.addAdjustmentListener(this);
 		barra2.addAdjustmentListener(this);
 		barra3.addAdjustmentListener(this);
@@ -34,7 +46,10 @@ public class AppletDeslizadores extends Applet implements AdjustmentListener {
 	}
 
 	public void adjustmentValueChanged(AdjustmentEvent arg0) {
-			color = new Color(barra1.getValue(), barra2.getValue(), barra3.getValue());
-			fondo.setBackground(color);
+		color = new Color(barra1.getValue(), barra2.getValue(), barra3.getValue());
+		fondo.setBackground(color);
+		deslizador1.setBackground(color);
+		deslizador2.setBackground(color);
+		deslizador3.setBackground(color);
 	}
 }
